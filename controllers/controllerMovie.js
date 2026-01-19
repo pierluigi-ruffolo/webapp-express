5bea3d080909bec818d15e58005cbe9c2e36a8d6
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-01-19 15:44:58 +0100

    feat: implemento show

--- controllers/controllerMovie.js
+++ controllers/controllerMovie.js
@@ -13,7 +13,36 @@ function index(req, res) {
 }
 
 function show(req, res) {
-  res.send("sono show");
+  const id = req.params.id;
+  const sqlMovies = "SELECT * FROM `movies` WHERE `movies`.`id` = ?";
+  connection.query(sqlMovies, [id], (error, resultMovies) => {
+    if (error) {
+      return res.status(500).json({
+        message: "Error Server",
+      });
+    }
+    if (resultMovies.length === 0) {
+      return res.status(404).json({
+        message: "Risorsa Non trovata",
+        error: "NOT FOUND",
+      });
+    }
+    const movies = resultMovies[0];
+    const sqlReviews = "SELECT * FROM `reviews` WHERE `reviews`.`movie_id` = ?";
+    connection.query(sqlReviews, [id], (error, resultReviews) => {
+      if (error) {
+        return res.status(500).json({
+          message: "Error Server",
+        });
+      }
+      const response = {
+        ...movies,
+        reviews: resultReviews,
+      };
+
+      res.json(response);
+    });
+  });
 }
 
 const controller = {
